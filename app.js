b6151fbeba5016207c6175a60c28878467e6671d
My Ho <mydiemho@gmail.com>
2015-02-07 15:20:53 -0800

    allow user to delete items

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 var itemTemplate = $('#templates .item')
 var list         = $('#list')
+var form         = $('#add-form')
 
 var addItemToPage = function(itemData) {
-    console.log(itemData)
     var item = itemTemplate.clone()
-    console.log(item)
     item.attr('data-id', itemData.id)
     item.find('.description').text(itemData.description)
     if(itemData.completed) {
         item.addClass('completed')
     }
 
-    console.log("before append:", item)
     list.append(item)
+
+    // reset form to blank
+    form.find('#create').val("")
 }
 
 var loadRequest = $.ajax({
@@ -20,6 +21,7 @@ var loadRequest = $.ajax({
     url: 'https://listalous.herokuapp.com/lists/mydiemho'
 })
 
+// read data from server and populate list
 loadRequest.done(function(dataFromServer) {
     var itemsData = dataFromServer.items
 
@@ -28,9 +30,11 @@ loadRequest.done(function(dataFromServer) {
     })
 })
 
+// add item to list when user enter on form
 $('#add-form').on('submit', function(event) {
-    itemDescription = event.target.itemDescription.value
     event.preventDefault()
+
+    var itemDescription = event.target.itemDescription.value
 
     var creationRequest = $.ajax({
         type: 'POST',
@@ -43,9 +47,12 @@ $('#add-form').on('submit', function(event) {
     })
 })
 
+// grayed-out item when user click on check
 $('#list').on('click', '.complete-button', function(event) {
+    event.preventDefault()
+
     var item = $(event.target).parent()
-    isItemCompleted = item.hasClass('completed')
+    var isItemCompleted = item.hasClass('completed')
     var itemId = item.attr('data-id')
 
     var updateRequest = $.ajax({
@@ -60,5 +67,22 @@ $('#list').on('click', '.complete-button', function(event) {
         } else {
             item.removeClass('completed')
         }
+    })
+})
+
+// delete item when user click on X
+$('#list').on('click', '.delete-button', function(event) {
+    event.preventDefault()
+
+    var item = $(event.target).parent()
+    var itemId = item.attr('data-id')
+
+    var deleteRequest = $.ajax({
+        type: 'DELETE',
+        url: 'https://listalous.herokuapp.com/lists/mydiemho/items/' + itemId
+    })
+
+    deleteRequest.done(function(itemDataFromServer) {
+        list.find('[data-id=' + itemId + ']').remove()
     })
 })
